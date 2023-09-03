ac1be87db9dc12c77d8663ce048f214435fd6c45
phukon <hello@rikiphukon.com>
2023-09-03 08:49:25 +0530

    Removed Link from CommunityBox and Added Plugin

--- xongroh-frontend/src/App.jsx
+++ xongroh-frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import { Routes, Route } from 'react-router-dom'
-import LoginPage from '@/scenes/loginPage'
-import RegisterPage from '@/scenes/registerPage'
+// import LoginPage from '@/scenes/loginPage'
+// import RegisterPage from '@/scenes/registerPage'
 import HomePage from '@/scenes/homePage/HomePage'
+import PostPage from '@/scenes/postPage/PostPage'
 import PageNotFound from '@/scenes/pageNotFound/PageNotFound'
 import SearchPage from '@/scenes/searchPage/SearchPage'
 import MyCommunities from '@/scenes/communities/MyCommunities'
@@ -11,14 +12,17 @@ import Layout from '@/scenes/Layout'
 import Prefetch from '@/features/auth/Prefetch'
 import DashLayout from '@/components/DashLayout'
 import Welcome from '@/features/auth/Welcome'
+import Public from '@/components/Public'
+import AuthenticationPage from '@/scenes/onboard'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Public />} />
-        <Route path="login" element={<LoginPage />} />
-        <Route path="register" element={<RegisterPage />} />
+        <Route index element={<AuthenticationPage />} />
+        {/* <Route path="login" element={<LoginPage />} />
+        <Route path="register" element={<RegisterPage />} /> */}
+
         <Route element={<Prefetch />}>
           <Route path="dash" element={<DashLayout />}>
             <Route index element={<Welcome />} />
